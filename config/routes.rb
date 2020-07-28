c317388cb297b64c0e90a0e06458a07dfb380fb0
codewithirene567 <irenejinscott@gmail.com>
2020-07-28 19:22:37 -0400

    made my users controller and also made my users/homepage my root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :animals  do
+  root 'users#home'
+  resources :animals  do
   resources :reasons, only: [:new, :create, :index] 
 end
 
