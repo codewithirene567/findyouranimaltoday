e86d144ced1224a5c8d493308abb071f6cf6ec42
codewithirene567 <irenejinscott@gmail.com>
2020-07-29 11:08:23 -0400

    wrote logic for my users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,28 @@
 class UsersController < ApplicationController
     def new
+      @user = User.new
     end
+
     def create
+      if (user = User.create(user_params))
+        session[:user_id] = user.user_id
+        redirect_to user_path(user)
+      else
+        render 'sessions/new'
+      end
     end
-    def destroy
+
+    def show
+      @user = User.find_by(id: params[:id])
     end
+
     def home
       render 'users/homepage'
     end 
+
+    private
+    
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
 end
