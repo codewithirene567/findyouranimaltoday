60012fbdd80909549d0b3c33d1352cd12f990b7a
codewithirene567 <irenejinscott@gmail.com>
2020-07-29 10:32:06 -0400

    updated my routes so that they are organized better, also modified the spec.md file after meeting some requirements

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :animals  do
   resources :reasons, only: [:new, :create, :index] 
 end
+resources :sessions, only: [:new, :create, :destroy]
 
 resources :users, only: [:new, :create, :show]
-resources :sessions, only: [:create, :destroy]
+
+
 
 end
 
