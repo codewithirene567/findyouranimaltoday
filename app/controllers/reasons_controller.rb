48bbe2256d9483a2f3d746bf8e868ea50e4f3342
codewithirene567 <irenejinscott@gmail.com>
2020-08-02 17:13:25 -0400

    got rid of comments I didn't need in the reasons controller

--- app/controllers/reasons_controller.rb
+++ app/controllers/reasons_controller.rb
@@ -12,26 +12,16 @@ class ReasonsController < ApplicationController
 
     def create
       animal = Animal.find_by(id: params[:reason][:animal_id])
-      #reason = animal.reasons.build(reason_params)
-      
       reason = current_user.reasons.build(reason_params)
-      
-      #Reason.all.find_by(id: params[:animal_id])
-      #current_user.reasons.animals << reason
       current_user.reasons << reason
-      #current_user.animals.find_by(id: params[:id]).object_id
-      #if object_id.exists?
-
-      if reason.save
-      #redirect_to animals_path
-      redirect_to animal_reasons_path(animal)
-      else
-       render 'reasons/new'
-      end
+        if reason.save
+           redirect_to animal_reasons_path(animal)
+        else
+           render 'reasons/new'
+        end
     end
 
     def index
-        #@reasons = Reason.all
         @animal = Animal.find_by(id: params[:animal_id])
         if @animal
           @reasons = @animal.reasons
@@ -41,10 +31,10 @@ class ReasonsController < ApplicationController
     end
 
     
-    private
+  private
 
   def reason_params
     params.require(:reason).permit(:user_id, :animal_id, :words, animal_attributes: [:name])
-    #params.require(:reason).permit(:user_id, :animal_id, :words, animal_attributes: [:name])
   end
+
 end
