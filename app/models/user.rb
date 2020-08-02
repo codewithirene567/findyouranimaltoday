018d12e1f0ba146d7ba280f8320cf329362a8f67
codewithirene567 <irenejinscott@gmail.com>
2020-08-02 17:18:35 -0400

    got rid of comments I didn't need in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,16 @@
 class User < ApplicationRecord
     has_many :reasons
     has_many :animals, through: :reasons
-    #has many animals is going to represent 
     has_secure_password
-    #has_securepassword has a validation for password presence
-    #has_many :favorite_animals, through: :reasons, source: :animal
-
-    #validates :name, uniqueness: true
+    #has_secure_password has a validation for password presence
+    
     validates :name, presence: true
-    #validates :password, presence: true
+
     def self.create_from_omniauth(auth)
         User.find_or_create_by(uid: auth['uid'], provider: auth['provider']) do |u|
             u.name = auth['info']['first_name']
             u.password = SecureRandom.hex(16)
         end
     end
+
 end
