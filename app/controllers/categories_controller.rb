9f07d9a74f011307c977c7f799a68b38e7895c0d
codewithirene567 <irenejinscott@gmail.com>
2020-08-02 17:10:53 -0400

    got rid of comments I didn't need in the categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,11 @@
 class CategoriesController < ApplicationController
     before_action :checked_log_in
+    
     def new
         @category = Category.new
     end
 
     def create
-        #binding.pry
-        #@category = Category.new(category_params)
-        #@animal = Animal.find_by(id: params[:id])
-        #@category = @animal.build_category(category_params)
-        #for a belongs to relationship you should use a _category with an underscore
-        #@animal = animal.category
         @category = Category.create(category_params)
             if @category.save
                 redirect_to category_path(@category)
@@ -27,4 +22,5 @@ class CategoriesController < ApplicationController
     def category_params
         params.require(:category).permit(:value, :animals)
     end
+    
 end
